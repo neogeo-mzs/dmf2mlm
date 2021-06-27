14c32acfb02facfb8aa1d199f324d454289e8221
StereoMimi <42147021+stereomimi@users.noreply.github.com>
2021-06-27 20:40:23 +0200

    Made instrument and other data classes

--- mzs/__main__.py
+++ mzs/__main__.py
@@ -1,7 +1,7 @@
 from event import *
+from enum import Enum, IntEnum
 
 ######################## SONGS ########################
-
 
 class SongChannel:
 	event_list: [SongEvent]
@@ -9,6 +9,63 @@ class SongChannel:
 class Song:
 	channels: [SongChannel]
 	sub_event_lists: [[SongEvent]]
+
+
+######################## OTHER DATA ########################
+
+class OtherDataIndex(int):
+	pass
+
+class OtherData:
+	pass
+
+class SampleList(OtherData):
+	start_addresses: [int]
+	end_addresses: [int]
+
+class SSGMacro(OtherData):
+	length: int
+	loop_point: int
+	data: bytearray
+	
+######################## INSTRUMENTS ########################
+
+class Instrument:
+	pass
+
+class ADPCMAInstrument(Instrument):
+	sample_list: OtherDataIndex
+
+class FMOperator:
+	dtmul: int
+	tl: int
+	ksar: int
+	amdr: int
+	sr: int
+	slrr: int
+	eg: int
+
+class FMInstrument(Instrument):
+	fbalgo: int
+	amspms: int
+	op_enable: [bool] # [OP1, OP2, OP3, OP4] Enable
+	operators: [FMOperator]
+
+class SSGMixing(IntEnum):
+	NONE  = 0
+	TONE  = 1
+	NOISE = 2
+	BOTH  = 3
+
+class SSGInstrument(Instrument):
+	mixing: SSGMixing
+	eg_enable: bool
+	volenv_period_fine_tune: int
+	volenv_period_coarse_tune: int
+	volenv_shape: int
+	mix_macro: OtherDataIndex
+	vol_macro: OtherDataIndex
+	arp_macro: OtherDataIndex
 
 ######################## SOUND DATA ########################
 

2dec200bff5e1c5920863d66b56698176b0f947a
StereoMimi <42147021+stereomimi@users.noreply.github.com>
2022-01-29 15:58:57 +0100

    include 68k/z80 com. lib in sfx header

--- src/sfx.py
+++ src/sfx.py
@@ -1,5 +1,62 @@
 from pathlib import Path
 
+C_HEADER_BEGIN = """/*
+    ==============[SFX CONSTS]==============
+        Header generated using 'dmf2mlm'  
+      https://github.com/GbaCretin/dmf2mlm
+    ========================================
+*/
+
+#ifndef DMF2MLM_Z80_CODE_H
+#define DMF2MLM_Z80_CODE_H
+
+#define Z80_UCOM_PLAY_SONG(song)              Z80_send_user_command(0x01, song)
+#define Z80_UCOM_STOP()                       Z80_send_user_command(0x02, 0)
+#define Z80_UCOM_BUFFER_SFXPS_CVOL(pan, cvol) Z80_send_user_command(0x03, (pan) | ((cvol) & 0x1F))
+#define Z80_UCOM_BUFFER_SFXPS_PRIO(prio)      Z80_send_user_command(0x04, prio)
+#define Z80_UCOM_PLAY_SFXPS_SMP(smp)          Z80_send_user_command(0x05, smp)
+#define Z80_UCOM_FADE_IN(tmb_ldcnt)           Z80_send_user_command(0x06 | (tmb_ldcnt & 1), tmb_ldcnt>>1)
+#define Z80_UCOM_FADE_OUT(tmb_ldcnt)          Z80_send_user_command(0x08 | (tmb_ldcnt & 1), tmb_ldcnt>>1)
+#define Z80_UCOM_BUFFER_FADE_OFS(ofs)         Z80_send_user_command(0x0A, ofs)
+
+typedef enum {
+    PAN_NONE   = 0x00,
+    PAN_LEFT   = 0x40,
+    PAN_RIGHT  = 0x20,
+    PAN_CENTER = 0x60,
+} Panning;
+
+// Both the command's and the
+// parameter's MSB is always
+// set to 1
+void Z80_send_user_command(u8 command, u8 parameter);
+#endif // DMF2MLM_Z80_CODE_H
+
+#ifdef DMF2MLM_Z80_IMPLEMENTATION
+void __attribute__((optimize("O0"))) Z80_wait_loop(int loops)
+{
+    for(u16 i = 0; i < loops; ++i);
+}
+
+void Z80_send_user_command(u8 command, u8 parameter)
+{
+	const u8 user_com_mask = 0x80;
+	command |= user_com_mask;
+	parameter |= user_com_mask;
+
+	*REG_SOUND = command;
+    u8 neg_command = command ^ 0xFF;
+	while (*REG_SOUND != neg_command);
+	wait_loop(64);
+
+	*REG_SOUND = parameter;
+    u8 neg_parameter = parameter ^ 0xFF;
+	while (*REG_SOUND != neg_parameter);
+	wait_loop(64);
+}
+#endif // DMF2MLM_Z80_IMPLEMENTATION
+
+"""
 class SFXSamples:
     MAX_SAMPLE_COUNT = 128
     paths: [Path]
@@ -15,7 +72,7 @@ class SFXSamples:
 
     def generate_c_header(self) -> str:
         CONST_PREFIX = "SFX_"
-        c_header = "/*\n  ============[SFX CONSTS]============\n  Header generated using 'dmf2mlm'\n  https://github.com/GbaCretin/dmf2mlm\n*/\n\n"
+        c_header = C_HEADER_BEGIN
 
         for i in range(len(self.paths)):
             sfx_name = self.paths[i].name.removesuffix(".raw")

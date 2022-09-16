5b4f0eff8e9dc4d46fdb5a5fdd0035ead9523c0f
StereoMimi <42147021+stereomimi@users.noreply.github.com>
2022-09-16 15:57:47 +0200

    new hader

--- src/sfx.py
+++ src/sfx.py
@@ -3,59 +3,77 @@ from pathlib import Path
 C_HEADER_BEGIN = """/*
     ==============[SFX CONSTS]==============
         Header generated using 'dmf2mlm'  
-      https://github.com/GbaCretin/dmf2mlm
+      https://github.com/StereoMimi/dmf2mlm
     ========================================
 */
 
-#ifndef DMF2MLM_Z80_CODE_H
-#define DMF2MLM_Z80_CODE_H
+#ifndef MZS_H
+#define MZS_H
 
-#define Z80_UCOM_PLAY_SONG(song)              Z80_send_user_command(0x01, song)
-#define Z80_UCOM_STOP()                       Z80_send_user_command(0x02, 0)
-#define Z80_UCOM_BUFFER_SFXPS_CVOL(pan, cvol) Z80_send_user_command(0x03, (pan) | ((cvol) & 0x1F))
-#define Z80_UCOM_BUFFER_SFXPS_PRIO(prio)      Z80_send_user_command(0x04, prio)
-#define Z80_UCOM_PLAY_SFXPS_SMP(smp)          Z80_send_user_command(0x05, smp)
-#define Z80_UCOM_RETRIG_SFXPS_SMP(smp)        Z80_send_user_command(0x0B, smp)
-#define Z80_UCOM_FADE_IN(tmb_ldcnt)           Z80_send_user_command(0x06 | (tmb_ldcnt & 1), tmb_ldcnt>>1)
-#define Z80_UCOM_FADE_OUT(tmb_ldcnt)          Z80_send_user_command(0x08 | (tmb_ldcnt & 1), tmb_ldcnt>>1)
-#define Z80_UCOM_BUFFER_FADE_OFS(ofs)         Z80_send_user_command(0x0A, ofs)
+#define MZS_UCOM_PLAY_SONG(song)              MZS_send_user_command(0x01, song)
+#define MZS_UCOM_STOP()                       MZS_send_user_command(0x02, 0)
+#define MZS_UCOM_BUFFER_SFXPS_CVOL(pan, cvol) MZS_send_user_command(0x03, (pan) | ((cvol) & 0x1F))
+#define MZS_UCOM_BUFFER_SFXPS_PRIO(prio)      MZS_send_user_command(0x04, prio)
+#define MZS_UCOM_PLAY_SFXPS_SMP(smp)          MZS_send_user_command(0x05, smp)
+#define MZS_UCOM_SET_MLM_MVOL(mvol)           MZS_send_user_command(0x06 | (mvol & 1), mvol >> 1)
+#define MZS_UCOM_SET_FADE(ofs)                MZS_send_user_command((0x08 | ((ofs >> 7) & 1)), ofs & 0x7F)
+#define MZS_UCOM_RETRIG_SFXPS_SMP(smp)        MZS_send_user_command(0x0B, smp)
 
 typedef enum {
     PAN_NONE   = 0x00,
     PAN_LEFT   = 0x40,
     PAN_RIGHT  = 0x20,
     PAN_CENTER = 0x60,
-} Panning;
+} MZS_Panning;
 
-// Both the command's and the
-// parameter's MSB is always
-// set to 1
-void Z80_send_user_command(u8 command, u8 parameter);
-#endif // DMF2MLM_Z80_CODE_H
+void MZS_send_user_command(unsigned char command, unsigned char parameter);
 
-#ifdef DMF2MLM_Z80_IMPLEMENTATION
-void __attribute__((optimize("O0"))) Z80_wait_loop(int loops)
+inline void MZS_UCOM_play_song(unsigned char song)
+{ MZS_send_user_command(0x01, song); }
+
+inline void MZS_UCOM_stop_song()
+{ MZS_send_user_command(0x02, 0); }
+
+inline void MZS_UCOM_buffer_sfxps_cvol(MZS_Panning pan, unsigned char cvol)
+{ MZS_send_user_command(0x03, (pan) | ((cvol) & 0x1F)); }
+
+inline void MZS_UCOM_buffer_sfxps_prio(unsigned char prio)
+{ MZS_send_user_command(0x04, prio); }
+
+inline void MZS_UCOM_play_sfxps_smp(unsigned char smp)
+{ MZS_send_user_command(0x05, smp); }
+
+inline void MZS_UCOM_set_mlm_vol(unsigned char mvol)
+{ MZS_send_user_command(0x06 | (mvol & 1), mvol >> 1); }
+
+inline void MZS_UCOM_set_fade(signed char fade)
+{ MZS_send_user_command(0x08 | *((unsigned char*)(&fade)) >> 7, *((unsigned char*)(&fade)) & 0x7F); }
+
+#ifdef MZS_IMPLEMENTATION
+void __attribute__((optimize("O0"))) __MZS_wait_loop(int loops)
 {
-    for(u16 i = 0; i < loops; ++i);
+    for(unsigned short i = 0; i < loops; ++i);
 }
 
-void Z80_send_user_command(u8 command, u8 parameter)
+void MZS_send_user_command(unsigned char command, unsigned char parameter)
 {
-	const u8 user_com_mask = 0x80;
-	command |= user_com_mask;
-	parameter |= user_com_mask;
+    const unsigned char user_com_mask = 0x80;
+    command |= user_com_mask;
+    parameter |= user_com_mask;
 
-	*REG_SOUND = command;
-    u8 neg_command = command ^ 0xFF;
-	while (*REG_SOUND != neg_command);
-	wait_loop(64);
+    *((volatile unsigned char*)0x320000) = command;
+    unsigned char neg_command = command ^ 0xFF;
+    while (*((volatile unsigned char*)0x320000) != neg_command);
+    __MZS_wait_loop(64);
 
-	*REG_SOUND = parameter;
-    u8 neg_parameter = parameter ^ 0xFF;
-	while (*REG_SOUND != neg_parameter);
-	wait_loop(64);
+    *((volatile unsigned char*)0x320000) = parameter;
+    unsigned char neg_parameter = parameter ^ 0xFF;
+    while (*((volatile unsigned char*)0x320000) != neg_parameter);
+    __MZS_wait_loop(64);
 }
-#endif // DMF2MLM_Z80_IMPLEMENTATION
+#endif
+
+#endif
 
 """
 class SFXSamples:
